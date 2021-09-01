5c85adafd506831a206914ecccff934f0fae0af4
Rahat <rahat392@gmail.com>
2021-09-01 13:37:59 +0600

    finshing index.php

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use Rahat\FiciFirebaseServer;
-
-
+use Rahat\FiciFirebaseServer\CloudMessaging;
 
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
     exit;
 }
+
+$messaging = new CloudMessaging();
+
+$result = $messaging
+    ->setNotification($_POST['title'], $_POST['body'])
+    ->setTopic($_POST['topic'])
+    ->setData($_POST['data'])
+    ->send();
+
+header('Content-Type: application/json');
+echo json_encode($result);
